06438a62cb5d1fd6d43bd35272baf55346ef19eb
Muhiuddin Dewan <mohidewan10@gmail.com>
2023-07-31 16:58:10 +0600

    app deployment using user data file

--- terraform_manifest/ec2-instance.tf
+++ terraform_manifest/ec2-instance.tf
@@ -1,5 +1,8 @@
-# Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0d13e3e640877b0b9" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-0d13e3e640877b0b9"
   instance_type = "t2.micro"
+  user_data     = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "app-1"
+  }
 }
